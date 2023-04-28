51edd18546e72f8467cc4864773f5f2ace176491
GET Academy <terje@getacademy.no>
2023-04-28 10:53:25 +0200

    x

---
+++ TemplateMethodEnumerableAndMore/GenereltOmTemplate/MyBaseClass.cs
@@ -0,0 +1,13 @@
+﻿namespace TemplateMethodEnumerableAndMore.GenereltOmTemplate
+{
+    internal class MyBaseClass
+    {
+
+        public virtual void DoIt()
+        {
+            Console.WriteLine("Main thing 1");
+            Console.WriteLine("Main thing 2");
+            Console.WriteLine("Main thing 3");
+        }
+    }
+}
---
+++ TemplateMethodEnumerableAndMore/GenereltOmTemplate/MySubClassA.cs
@@ -0,0 +1,12 @@
+﻿namespace TemplateMethodEnumerableAndMore.GenereltOmTemplate
+{
+    internal class MySubClassA : MyBaseClass
+    {
+        public override void DoIt()
+        {
+            Console.WriteLine("A thing before");
+            base.DoIt();
+            Console.WriteLine("A thing after");
+        }
+    }
+}
